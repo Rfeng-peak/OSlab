c8bcdac33c18de314f17ac58cdfc3a98cfee14c0
Rfeng-peak <wangjunfeng23@mails.ucas.ac.cn>
2026-07-26 10:57:25 +0800

    Add files via upload

---
+++ init/main.c
@@ -0,0 +1,239 @@
+#include <common.h>
+#include <asm.h>
+#include <asm/unistd.h>
+#include <os/loader.h>
+#include <os/irq.h>
+#include <os/sched.h>
+#include <os/lock.h>
+#include <os/kernel.h>
+#include <os/task.h>
+#include <os/string.h>
+#include <os/mm.h>
+#include <os/time.h>
+#include <sys/syscall.h>
+#include <screen.h>
+#include <printk.h>
+#include <assert.h>
+#include <type.h>
+#include <csr.h>
+#include <os/smp.h>
+
+extern void ret_from_exception();
+
+// Task info array
+task_info_t tasks[TASK_MAXNUM];
+
+
+static void init_jmptab(void)
+{
+    volatile long (*(*jmptab))() = (volatile long (*(*))())KERNEL_JMPTAB_BASE;
+
+    jmptab[CONSOLE_PUTSTR]  = (long (*)())port_write;
+    jmptab[CONSOLE_PUTCHAR] = (long (*)())port_write_ch;
+    jmptab[CONSOLE_GETCHAR] = (long (*)())port_read_ch;
+    jmptab[SD_READ]         = (long (*)())sd_read;
+    jmptab[SD_WRITE]        = (long (*)())sd_write;
+    jmptab[QEMU_LOGGING]    = (long (*)())qemu_logging;
+    jmptab[SET_TIMER]       = (long (*)())set_timer;
+    jmptab[READ_FDT]        = (long (*)())read_fdt;
+    jmptab[MOVE_CURSOR]     = (long (*)())screen_move_cursor;
+    jmptab[PRINT]           = (long (*)())printk;
+    jmptab[YIELD]           = (long (*)())do_scheduler;
+    jmptab[MUTEX_INIT]      = (long (*)())do_mutex_lock_init;
+    jmptab[MUTEX_ACQ]       = (long (*)())do_mutex_lock_acquire;
+    jmptab[MUTEX_RELEASE]   = (long (*)())do_mutex_lock_release;
+
+    // TODO: [p2-task1] (S-core) initialize system call table.
+
+}
+
+#define TASK_INFO_ADDR pa2kva(0x50200000)
+#define TASK_INFO_OFFSET 0x100
+#define TASK_NAME_LEN 32
+
+static void init_task_info(void)
+{
+    /* Read task names from boot sector (identity-mapped at 0x50200000) */
+    uint8_t count = *(uint8_t *)(TASK_INFO_ADDR + TASK_INFO_OFFSET);
+    if (count > TASK_MAXNUM)
+        count = TASK_MAXNUM;
+    char *ptr = (char *)(TASK_INFO_ADDR + TASK_INFO_OFFSET + 1);
+    for (int i = 0; i < (int)count; i++) {
+        strncpy(tasks[i].name, ptr, TASK_NAME_LEN - 1);
+        tasks[i].name[TASK_NAME_LEN - 1] = '\0';
+        tasks[i].id = i;
+        ptr += TASK_NAME_LEN;
+    }
+}
+
+/************************************************************/
+void init_pcb_stack(
+    ptr_t kernel_stack, ptr_t user_stack, ptr_t entry_point,
+    pcb_t *pcb)
+{
+     /* TODO: [p2-task3] initialization of registers on kernel stack
+      * HINT: sp, ra, sepc, sstatus
+      * NOTE: To run the task in user mode, you should set corresponding bits
+      *     of sstatus(SPP, SPIE, etc.).
+      */
+    regs_context_t *pt_regs =
+        (regs_context_t *)(kernel_stack - sizeof(regs_context_t));
+    memset(pt_regs, 0, sizeof(regs_context_t));
+    pt_regs->regs[2] = user_stack;
+    pt_regs->regs[4] = (reg_t)pcb;
+    pt_regs->sepc = entry_point;
+    pt_regs->sstatus = SR_SPIE;
+
+
+    /* TODO: [p2-task1] set sp to simulate just returning from switch_to
+     * NOTE: you should prepare a stack, and push some values to
+     * simulate a callee-saved context.
+     */
+    switchto_context_t *pt_switchto =
+        (switchto_context_t *)((ptr_t)pt_regs - sizeof(switchto_context_t));
+    memset(pt_switchto, 0, sizeof(switchto_context_t));
+    pt_switchto->regs[0] = (reg_t)ret_from_exception;
+    pt_switchto->regs[1] = (reg_t)pt_switchto;
+
+    pcb->kernel_sp = (ptr_t)pt_switchto;
+    pcb->user_sp = user_stack;
+
+}
+
+static void init_pcb(void)
+{
+    /* TODO: [p2-task1] load needed tasks and init their corresponding PCB */
+
+    /* For P4-Task1 we share the kernel page directory.  Later tasks
+     * will give each process its own pgdir.
+     */
+    uintptr_t shared_pgdir = PGDIR_PA;
+
+    int task_num = 1; /* start with just the shell */
+    for (int i = 0; i < task_num; i++) {
+        ptr_t kernel_stack = allocKernelPage(1) + PAGE_SIZE;
+        /* user space pre-mapped as 2MB large pages by setup_vm */
+        ptr_t user_stack = USER_STACK_ADDR + PAGE_SIZE;
+        ptr_t entry_point = load_task_img(i, shared_pgdir);
+
+        pcb[i].pid = process_id++;
+        pcb[i].status = TASK_READY;
+        pcb[i].cursor_x = 0;
+        pcb[i].cursor_y = i;
+        pcb[i].pgdir = shared_pgdir;
+        init_pcb_stack(kernel_stack, user_stack, entry_point, &pcb[i]);
+
+        init_list_head(&pcb[i].list);
+        init_list_head(&pcb[i].wait_list);
+        list_add_tail(&pcb[i].list, &ready_queue);
+    }
+
+
+    /* TODO: [p2-task1] remember to initialize 'current_running' */
+    init_list_head(&pid0_pcb.list);
+    pid0_pcb.status = TASK_RUNNING;
+    pid0_pcb.cursor_x = 0;
+    pid0_pcb.cursor_y = 0;
+    pid0_pcb.pgdir = PGDIR_PA;
+    current_running = &pid0_pcb;
+
+}
+
+static void init_syscall(void)
+{
+    // TODO: [p2-task3] initialize system call table.
+    syscall[SYSCALL_SLEEP] = (long (*)())do_sleep;
+    syscall[SYSCALL_YIELD] = (long (*)())do_scheduler;
+    syscall[SYSCALL_WRITE] = (long (*)())screen_write;
+    syscall[SYSCALL_CURSOR] = (long (*)())screen_move_cursor;
+    syscall[SYSCALL_REFLUSH] = (long (*)())screen_reflush;
+    syscall[SYSCALL_GET_TIMEBASE] = (long (*)())get_time_base;
+    syscall[SYSCALL_GET_TICK] = (long (*)())get_ticks;
+    syscall[SYSCALL_LOCK_INIT] = (long (*)())do_mutex_lock_init;
+    syscall[SYSCALL_LOCK_ACQ] = (long (*)())do_mutex_lock_acquire;
+    syscall[SYSCALL_LOCK_RELEASE] = (long (*)())do_mutex_lock_release;
+    syscall[SYSCALL_EXEC] = (long (*)())do_exec;
+    syscall[SYSCALL_EXIT] = (long (*)())do_exit;
+    syscall[SYSCALL_KILL] = (long (*)())do_kill;
+    syscall[SYSCALL_WAITPID] = (long (*)())do_waitpid;
+    syscall[SYSCALL_PS] = (long (*)())do_process_show;
+    syscall[SYSCALL_GETPID] = (long (*)())do_getpid;
+    syscall[SYSCALL_READCH] = (long (*)())bios_getchar;
+    syscall[SYSCALL_CLEAR] = (long (*)(long, long, long, long, long))screen_clear;
+    syscall[SYSCALL_SHOW_TASK] = (long (*)())do_process_show;
+}
+/************************************************************/
+
+/*
+ * Once a CPU core calls this function,
+ * it will stop executing!
+ */
+static void kernel_brake(void)
+{
+    disable_interrupt();
+    while (1)
+        __asm__ volatile("wfi");
+}
+
+int main(void)
+{
+    // Init jump table provided by kernel and bios(ΦωΦ)
+    init_jmptab();
+
+    // Init task information (〃'▽'〃)
+    init_task_info();
+
+    // Init Process Control Blocks |•'-'•) ✧
+    init_pcb();
+    printk("> [INIT] PCB initialization succeeded.\n");
+
+    // Read CPU frequency (｡•ᴗ-)_
+    time_base = bios_read_fdt(TIMEBASE);
+
+    // Init lock mechanism o(´^｀)o
+    init_locks();
+    printk("> [INIT] Lock mechanism initialization succeeded.\n");
+
+    // Init interrupt (^_^)
+    init_exception();
+    printk("> [INIT] Interrupt processing initialization succeeded.\n");
+
+    // Init system call table (0_0)
+    init_syscall();
+    printk("> [INIT] System call initialized successfully.\n");
+
+    // Init screen (QAQ)
+    init_screen();
+    printk("> [INIT] SCREEN initialization succeeded.\n");
+
+    /*
+     * Just start kernel with VM and print this string
+     * in the first part of task 1 of project 4.
+     * NOTE: if you use SMP, then every CPU core should call
+     *  `kernel_brake()` to stop executing!
+     */
+    printk("> [INIT] CPU #%u has entered kernel with VM!\n",
+        (unsigned int)get_current_cpu_id());
+
+    // TODO: [p2-task4] Setup timer interrupt and enable all interrupt globally
+    // NOTE: The function of sstatus.sie is different from sie's
+    // [p4-task1 cont.] start user processes under VM
+    bios_set_timer(get_ticks() + TIMER_INTERVAL);
+    enable_interrupt();
+    enable_preempt();
+    do_scheduler();
+
+
+    // TODO: Load tasks by either task id [p1-task3] or task name [p1-task4],
+    //   and then execute them.
+
+    while (1) {
+        bios_set_timer(get_ticks() + TIMER_INTERVAL);
+    enable_interrupt();
+    enable_preempt();
+    do_scheduler();
+        asm volatile("wfi");
+    }
+
+    return 0;
+}
---
+++ kernel/sched/sched.c
@@ -0,0 +1,301 @@
+#include <os/list.h>
+#include <os/lock.h>
+#include <os/kernel.h>
+#include <os/sched.h>
+#include <os/task.h>
+#include <os/time.h>
+#include <os/mm.h>
+#include <os/loader.h>
+#include <screen.h>
+#include <os/string.h>
+#include <printk.h>
+#include <assert.h>
+
+pcb_t pcb[NUM_MAX_TASK];
+const ptr_t pid0_stack = INIT_KERNEL_STACK + PAGE_SIZE;
+pcb_t pid0_pcb = {
+    .pid = 0,
+    .kernel_sp = (ptr_t)pid0_stack,
+    .user_sp = (ptr_t)pid0_stack
+};
+
+LIST_HEAD(ready_queue);
+LIST_HEAD(sleep_queue);
+
+/* global process id */
+pid_t process_id = 1;
+
+void do_scheduler(void)
+{
+    // TODO: [p2-task3] Check sleep queue to wake up PCBs
+    // check_sleeping(); // P4: skip pending investigation of sleep queue issue
+
+    /************************************************************/
+    /* Do not touch this comment. Reserved for future projects. */
+    /************************************************************/
+
+    // TODO: [p2-task1] Modify the current_running pointer.
+    if (list_empty(&ready_queue)) {
+        return;
+    }
+
+    pcb_t *prev_running = current_running;
+    if (prev_running != &pid0_pcb && prev_running->status == TASK_RUNNING) {
+        prev_running->status = TASK_READY;
+        list_add_tail(&prev_running->list, &ready_queue);
+    }
+
+    pcb_t *next_running = list_entry(ready_queue.next, pcb_t, list);
+    list_del(&next_running->list);
+    next_running->status = TASK_RUNNING;
+    current_running = next_running;
+
+    // TODO: [p2-task1] switch_to current_running
+    switch_to(prev_running, next_running);
+
+}
+
+void do_sleep(uint32_t sleep_time)
+{
+    // TODO: [p2-task3] sleep(seconds)
+    // NOTE: you can assume: 1 second = 1 `timebase` ticks
+    // 1. block the current_running
+    // 2. set the wake up time for the blocked task
+    // 3. reschedule because the current_running is blocked.
+    current_running->wakeup_time = get_timer() + sleep_time;
+    do_block(&current_running->list, &sleep_queue);
+}
+
+void do_block(list_node_t *pcb_node, list_head *queue)
+{
+    // TODO: [p2-task2] block the pcb task into the block queue
+    pcb_t *pcb_blocked = list_entry(pcb_node, pcb_t, list);
+    pcb_blocked->status = TASK_BLOCKED;
+    list_add_tail(pcb_node, queue);
+    do_scheduler();
+}
+
+void do_unblock(list_node_t *pcb_node)
+{
+    // TODO: [p2-task2] unblock the `pcb` from the block queue
+    pcb_t *pcb_unblocked = list_entry(pcb_node, pcb_t, list);
+    list_del(pcb_node);
+    pcb_unblocked->status = TASK_READY;
+    list_add_tail(pcb_node, &ready_queue);
+}
+
+/* P3-TASK1: process management helpers */
+pid_t do_exec(long id_or_ptr, int argc, uint64_t arg0, uint64_t arg1, uint64_t arg2)
+{
+    int id;
+
+    if (id_or_ptr >= 0 && id_or_ptr < TASK_MAXNUM) {
+        id = (int)id_or_ptr;
+    } else if (id_or_ptr != 0) {
+        /* treat as user-space pointer to task name */
+        char buf[32];
+        const char *src = (const char *)id_or_ptr;
+        int i;
+        for (i = 0; i < (int)sizeof(buf) - 1; i++) {
+            buf[i] = src[i];
+            if (buf[i] == '\0') break;
+        }
+        buf[i] = '\0';
+        id = -1;
+        for (i = 0; i < TASK_MAXNUM; i++) {
+            if (tasks[i].name[0] && strcmp(tasks[i].name, buf) == 0) {
+                id = tasks[i].id;
+                break;
+            }
+        }
+        if (id < 0) return -1;
+    } else {
+        return -1;
+    }
+
+    /* find a free pcb slot */
+    int idx = -1;
+    for (int i = 1; i < NUM_MAX_TASK; i++) {
+        if (pcb[i].pid == 0 || pcb[i].status == TASK_EXITED) {
+            idx = i;
+            break;
+        }
+    }
+    if (idx == -1)
+        return -1;
+
+    /* Share kernel page table (user space pre-mapped by setup_vm) */
+    uintptr_t pgdir = PGDIR_PA;
+
+    ptr_t kernel_stack = allocKernelPage(1) + PAGE_SIZE;
+    uint64_t ustack_va = USER_STACK_ADDR + idx * PAGE_SIZE;
+    alloc_page_helper(ustack_va, pgdir);
+    ptr_t user_stack = ustack_va + PAGE_SIZE;
+    ptr_t entry_point = load_task_img(id, pgdir);
+
+    pcb[idx].pid = process_id++;
+    pcb[idx].status = TASK_READY;
+    pcb[idx].cursor_x = 0;
+    pcb[idx].cursor_y = idx;
+    pcb[idx].pgdir = pgdir;
+    init_list_head(&pcb[idx].list);
+    init_list_head(&pcb[idx].wait_list);
+    init_pcb_stack(kernel_stack, user_stack, entry_point, &pcb[idx]);
+
+    /* construct default argv on user stack for argument-dependent programs */
+    {
+        regs_context_t *ur = (regs_context_t *)(pcb[idx].kernel_sp + sizeof(switchto_context_t));
+        uintptr_t usp = ur->regs[2];
+        usp = ROUNDDOWN(usp - 128, 16);
+        char *stk = (char *)usp;
+        uintptr_t *argv_ptrs = (uintptr_t *)(stk + 64);
+        /* For rw (id==3): pass writable addresses converted to hex strings.
+         * For others: pass print_location as argv[1]. */
+        if (id == 3) {
+            /* provide 2 writable addresses on stack */
+            char *buf1 = stk, *buf2 = stk + 24;
+            ptr_t addr1 = usp + 128, addr2 = usp + 136;
+            /* format as hex strings */
+            buf1[0]='0'; buf1[1]='x';
+            int v = (int)(addr1 >> 28) & 0xF; buf1[2]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr1 >> 24) & 0xF; buf1[3]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr1 >> 20) & 0xF; buf1[4]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr1 >> 16) & 0xF; buf1[5]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr1 >> 12) & 0xF; buf1[6]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr1 >> 8) & 0xF;  buf1[7]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr1 >> 4) & 0xF;  buf1[8]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr1) & 0xF;       buf1[9]=(v<10)?(v+'0'):(v-10+'a');
+            buf1[10] = 0;
+            buf2[0]='0'; buf2[1]='x';
+            v = (int)(addr2 >> 28) & 0xF; buf2[2]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr2 >> 24) & 0xF; buf2[3]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr2 >> 20) & 0xF; buf2[4]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr2 >> 16) & 0xF; buf2[5]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr2 >> 12) & 0xF; buf2[6]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr2 >> 8) & 0xF;  buf2[7]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr2 >> 4) & 0xF;  buf2[8]=(v<10)?(v+'0'):(v-10+'a');
+            v = (int)(addr2) & 0xF;       buf2[9]=(v<10)?(v+'0'):(v-10+'a');
+            buf2[10] = 0;
+            argv_ptrs[0] = (uintptr_t)buf1;  /* argv[0] = addr1 hex */
+            argv_ptrs[1] = (uintptr_t)buf2;  /* argv[1] = addr2 hex */
+            argv_ptrs[2] = 0;
+            ur->regs[10] = 2;  /* argc = 2 (program name + 1 arg) */
+        } else {
+            stk[0]='p'; stk[1]=0; stk[2]='2'; stk[3]=0;
+            argv_ptrs[0] = usp;
+            argv_ptrs[1] = usp + 2;
+            argv_ptrs[2] = 0;
+            ur->regs[10] = 2;
+        }
+        ur->regs[11] = (reg_t)argv_ptrs;
+        ur->regs[2]  = usp;
+    }
+    list_add_tail(&pcb[idx].list, &ready_queue);
+    asm volatile("fence.i" ::: "memory");
+    bios_putchar('X');
+
+    return pcb[idx].pid;
+}
+
+void do_exit(void)
+{
+    /* mark current as exited and wake up waiters */
+    current_running->status = TASK_EXITED;
+
+    /* wake up all waiting PCBs */
+    while (!list_empty(&current_running->wait_list)) {
+        list_node_t *node = current_running->wait_list.next;
+        list_del(node);
+        pcb_t *p = list_entry(node, pcb_t, list);
+        p->status = TASK_READY;
+        list_add_tail(&p->list, &ready_queue);
+    }
+
+    do_scheduler();
+}
+
+int do_kill(pid_t pid)
+{
+    if (pid <= 0)
+        return -1;
+    for (int i = 0; i < NUM_MAX_TASK; i++) {
+        if (pcb[i].pid == pid) {
+            if (pcb[i].status == TASK_EXITED)
+                return -1;
+            pcb[i].status = TASK_EXITED;
+            /* remove from any queue (list_del on self-linked node is a no-op) */
+            list_del(&pcb[i].list);
+            /* wake up waiters */
+            while (!list_empty(&pcb[i].wait_list)) {
+                list_node_t *node = pcb[i].wait_list.next;
+                list_del(node);
+                pcb_t *p = list_entry(node, pcb_t, list);
+                p->status = TASK_READY;
+                list_add_tail(&p->list, &ready_queue);
+            }
+            /* if killing self, yield immediately */
+            if (&pcb[i] == current_running) {
+                do_scheduler();
+            }
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int do_waitpid(pid_t pid)
+{
+    if (pid <= 0)
+        return -1;
+    pcb_t *target = NULL;
+    for (int i = 0; i < NUM_MAX_TASK; i++) {
+        if (pcb[i].pid == pid) {
+            target = &pcb[i];
+            break;
+        }
+    }
+    if (!target)
+        return -1;
+
+    if (target->status == TASK_EXITED)
+        return 0;
+
+    /* block current on target's wait_list */
+    do_block(&current_running->list, &target->wait_list);
+    return 0;
+}
+
+void do_process_show()
+{
+    printk("[PS] PID\tSTATUS\n");
+    for (int i = 0; i < NUM_MAX_TASK; i++) {
+        if (pcb[i].pid != 0) {
+            const char *s = "UNKNOWN";
+            switch (pcb[i].status) {
+            case TASK_BLOCKED:
+                s = "BLOCKED";
+                break;
+            case TASK_RUNNING:
+                s = "RUNNING";
+                break;
+            case TASK_READY:
+                s = "READY";
+                break;
+            case TASK_EXITED:
+                s = "EXITED";
+                break;
+            }
+            printk("[PS] %d\t%s\n", pcb[i].pid, s);
+        }
+    }
+}
+
+pid_t do_getpid()
+{
+    return current_running->pid;
+}
+
+int do_getchar()
+{
+    return bios_getchar();
+}
